84d6587f3bfdb57c50fd634ee6dcd489208e5878
Drunkiel <drunkielfel@gmail.com>
2024-08-01 20:39:39 +0200

    Adding support for controllers and some upgrades to item creation

--- Assets/Scripts/Item/ItemHolder.cs
+++ Assets/Scripts/Item/ItemHolder.cs
@@ -1,40 +1,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json;
-using System.IO;
 
 public class ItemHolder : SaveLoadSystem
 {
-    public List<ItemData> _allItems = new();
+    public List<ItemID> _allItems = new();
     public List<ItemID> _weaponItems = new();
     public List<ItemID> _armorItems = new();
     public List<ItemID> _collectableItems = new();
-    public List<Object> allobjects = new();
-    public List<Object> objects = new();
+    //public List<Object> allobjects = new();
+    public List<GameObject> itemPrefabs = new();
 
     void Start()
     {
-        Save(itemsSavePath);
+        // Save(itemsSavePath);
         
         Load(itemsSavePath);
     }
 
-    public override void Save(string path)
-    {
-        for (int i = 0; i < allobjects.Count; i++)
-        {
-            // Collect data to save
-            string jsonData = JsonConvert.SerializeObject(allobjects[i], Formatting.Indented, new JsonSerializerSettings
-            {
-                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
-                TypeNameHandling = TypeNameHandling.Auto,
-                PreserveReferencesHandling = PreserveReferencesHandling.None
-            });
+    // public override void Save(string path)
+    // {
+    //     for (int i = 0; i < allobjects.Count; i++)
+    //     {
+    //         // Collect data to save
+    //         string jsonData = JsonConvert.SerializeObject(allobjects[i], Formatting.Indented, new JsonSerializerSettings
+    //         {
+    //             ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+    //             TypeNameHandling = TypeNameHandling.Auto,
+    //             PreserveReferencesHandling = PreserveReferencesHandling.None
+    //         });
 
-            // Save data to file
-            File.WriteAllText($"{path}/{allobjects[i].name}.json", jsonData);
-        }
-    }
+    //         // Save data to file
+    //         File.WriteAllText($"{path}/{allobjects[i].name}.json", jsonData);
+    //     }
+    // }
 
     public override void Load(string path)
     {
@@ -56,21 +55,58 @@ public class ItemHolder : SaveLoadSystem
                     WeaponData _newWeapon = ScriptableObject.CreateInstance<WeaponData>();
                     JsonConvert.PopulateObject(saveFile, _newWeapon);
                     _newItem = _newWeapon;
+                    _weaponItems.Add(CreateWeaponItem(_newWeapon));
                     break;
                 
+                //Parse to armor
                 case ItemType.Armor:
-                    //Parse to armor
                     ArmorData _newArmor = ScriptableObject.CreateInstance<ArmorData>();
                     JsonConvert.PopulateObject(saveFile, _newArmor);
                     _newItem = _newArmor;
+                    _armorItems.Add(CreateArmorItem(_newArmor));
                     break;
             }
 
             //If empty drop warning
             if (_newItem == null)
+            {
                 Debug.LogWarning($"File {itemLocation} does not match any known data type.");
-
-            objects.Add(_newItem);
+                return;
+            }
         }
+
+        _allItems.AddRange(_weaponItems);
+        _allItems.AddRange(_armorItems);
+        _allItems.AddRange(_collectableItems);
+    }
+
+    private ItemID CreateWeaponItem(WeaponData _weaponData)
+    {
+        //Creating new item
+        ItemID _itemID = Instantiate(itemPrefabs[0], new Vector2(0, -2), Quaternion.identity).GetComponent<ItemID>();
+        WeaponItem _weaponItem = _itemID._weaponItem;
+
+        //Assigning data
+        _itemID._itemData = _weaponData._itemData;
+        _weaponItem.weaponType = _weaponData.weaponType;
+        _weaponItem.holdingType = _weaponData.holdingType;
+        _weaponItem.resizable = _weaponData.resizable;
+
+        _itemID.name = _weaponData._itemData.displayedName;
+        return _itemID;
+    }
+
+    private ItemID CreateArmorItem(ArmorData _armorData)
+    {
+        //Creating new item
+        ItemID _itemID = Instantiate(itemPrefabs[1], new Vector2(0, -2), Quaternion.identity).GetComponent<ItemID>();
+        ArmorItem _armorItem = _itemID._armorItem;
+
+        //Assigning data
+        _itemID._itemData = _armorData._itemData;
+        _armorItem.armorType = _armorData.armorType;
+
+        _itemID.name = _armorData._itemData.displayedName;
+        return _itemID;
     }
 }
