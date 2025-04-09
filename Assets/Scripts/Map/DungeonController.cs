7db8994931d510d60edd8a5f43a436a6f59f9480
Drunkiel <drunkielfel@gmail.com>
2025-04-09 21:17:15 +0200

    Updating dungeons

--- Assets/Scripts/Map/DungeonController.cs
+++ Assets/Scripts/Map/DungeonController.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DungeonController : MonoBehaviour
 {
     [SerializeField] private EntityController _coreController;
     public int timeLeft;
+    [SerializeField] private Slider timeSlider;
 
     private void Start()
     {
+        timeSlider.maxValue = timeLeft;
+        timeSlider.value = timeLeft;
+        _coreController._statistics._statsController.UpdateManaSlider(0, _coreController._statistics.maxMana, true);
+        _coreController._statistics.maxMana = timeLeft;
         StartCoroutine(UpdateTimer());
     }
 
@@ -18,7 +24,8 @@ public class DungeonController : MonoBehaviour
 
         yield return new WaitForSeconds(1f);
         timeLeft -= 1;
-        _coreController._statistics.mana = timeLeft; 
+        timeSlider.value = timeLeft;
+        _coreController._statistics.TakeMana(-1, true);
 
         StartCoroutine(UpdateTimer());
     }
