efcb738832cb776b04d4486be15ce2481e645b76
Drunkiel <drunkielfel@gmail.com>
2025-12-14 23:58:46 +0100

    Cleaning assets and adding new buildings

--- Assets/Scripts/Map/DungeonController.cs
+++ Assets/Scripts/Map/DungeonController.cs
@@ -3,31 +3,75 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
+public class Clock
+{
+    public int hours;
+    public int minutes;
+    public int seconds;
+
+    public int timeLeft;
+
+    public void Initialize()
+    {
+        timeLeft = GetTimeInSeconds();
+    }
+
+    public int GetTimeInSeconds()
+    {
+        return hours * 3600 + minutes * 60 + seconds;
+    }
+
+    public void UpdateClock(int deltaTime)
+    {
+        timeLeft -= deltaTime;
+
+        if (timeLeft < 0)
+            timeLeft = 0;
+
+        hours = timeLeft / 3600;
+        minutes = timeLeft % 3600 / 60;
+        seconds = timeLeft % 60;
+    }
+
+    public string DisplayTime()
+    {
+        if (hours > 0)
+            return $"{hours}h{minutes:00}m{seconds:00}s";
+        else if (minutes > 0)
+            return $"{minutes}m{seconds:00}s";
+        else
+            return $"{seconds}s";
+    }
+}
+
+
 public class DungeonController : MonoBehaviour
 {
     public static DungeonController instance;
     [SerializeField] private EntityController _coreController;
-    public int timeLeft;
+    public Clock _clock;
     [SerializeField] private Slider timeSlider;
     [SerializeField] private TMP_Text timeText;
 
     private void Start()
     {
         instance = this;
-        timeSlider.maxValue = timeLeft;
-        timeSlider.value = timeLeft;
+        _clock.timeLeft = _clock.GetTimeInSeconds();
+        timeSlider.maxValue = _clock.timeLeft;
+        timeSlider.value = timeSlider.maxValue;
         StartCoroutine(UpdateTimer());
     }
 
     private IEnumerator UpdateTimer()
     {
-        if (timeLeft <= 0)
+        if (_clock.timeLeft <= 0)
             yield break;
 
         yield return new WaitForSeconds(1f);
-        timeLeft -= 1;
-        timeSlider.value = timeLeft;
-        timeText.text = $"{timeLeft}s";
+        _clock.UpdateClock(1);
+        timeSlider.value = _clock.timeLeft;
+        timeText.text = _clock.DisplayTime();
 
         if (_coreController != null)
             _coreController._statistics.TakeMana(-1, true);
